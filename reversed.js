b830bc15b2a5631505124eddd569c5bedc77e9a8
One Minute Coding <coding.in.one.minute@gmail.com>
2025-11-13 19:05:19 +0530

    Add function to reverse a string in JavaScript

---
+++ reversed.js
@@ -0,0 +1,6 @@
+// Write a Program to reverse a string in JavaScript.
+function ReversedString(str){
+  return str.split("").reverse().join("");
+
+}
+console.log(ReversedString("One Minute Coding"));
