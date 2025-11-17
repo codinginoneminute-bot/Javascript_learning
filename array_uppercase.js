1f8dac8096a0268fe33e46e08e0df07a93cd26b5
One Minute Coding <coding.in.one.minute@gmail.com>
2025-11-17 21:05:24 +0530

    Create array_uppercase.js

---
+++ array_uppercase.js
@@ -0,0 +1,6 @@
+/*Convert an Array of Strings 
+to Uppercase in JavaScript? */
+
+const arr =["bannana","apple","orange"];
+const toupperarray=arr.map(str=>str.toUpperCase());
+console.log(toupperarray);
